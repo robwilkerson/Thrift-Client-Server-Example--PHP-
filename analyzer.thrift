7f029d7bab3d19c07b1f4c360e88459d3c31f15b
Rob Wilkerson <rob@robwilkerson.org>
2010-09-10 09:46:10 -0400

    Adding the thrift file that defines the client-server interface.

---
+++ analyzer.thrift
@@ -0,0 +1,28 @@
+namespace php  analyzer
+# 
+# EXAMPLE JAVA NAMESPACE
+# Not used in this example
+# 
+# namespace java org.robwilkerson.nlp.zemanta
+
+struct Freebase {
+  1: string topic,
+  2: string id,
+  3: double confidence,
+  4: list<string> categories
+}
+
+struct Dmoz {
+  1: string topic,
+  2: double confidence,
+  3: list<string> categories
+}
+
+struct ZemantaAnalysis {
+  1: list<Freebase> freebase,
+  2: list<Dmoz> dmoz
+}
+
+service zemanta {
+  ZemantaAnalysis analyze(1:string content)
+}
